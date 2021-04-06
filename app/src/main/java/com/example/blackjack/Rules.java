a61ccf76d47e878c75b948f481c34eb53283c5e2
Chomaister <ethan724@hotmail.com>
2021-04-05 23:33:08 -0700

    feat: Implementing tablayout with viewPages for UI

--- app/src/main/java/com/example/blackjack/Rules.java
+++ app/src/main/java/com/example/blackjack/Rules.java
@@ -1,18 +1,94 @@
 package com.example.blackjack;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.blackjack.game.Game;
+import com.google.android.material.tabs.TabLayout;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Rules extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
+
+        tabLayout = findViewById(R.id.tab_rules);
+        viewPager = findViewById(R.id.view_pager);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Rules");
+        arrayList.add("Basic Strategy");
+
+        // TabLayout is null, viewPager is not.
+        System.out.println("Tab Layout: " + tabLayout.getId());
+        prepareViewPager(viewPager, arrayList);
+       // System.out.println(tabLayout.getId());
+        System.out.println(viewPager.getId());
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void prepareViewPager(ViewPager viewPager, ArrayList<String> arrayList) {
+
+        MainAdapter adapter = new MainAdapter(getSupportFragmentManager());
+        MainFragment fragment = new MainFragment();
+
+        for(int i=0; i<arrayList.size(); i++){
+            Bundle bundle = new Bundle();
+            bundle.putString("title", arrayList.get(i));
+            fragment.setArguments(bundle);
+            adapter.addFragment(fragment, arrayList.get(i));
+            fragment = new MainFragment();
+        }
+        viewPager.setAdapter(adapter);
+    }
+
+    private class MainAdapter extends FragmentPagerAdapter {
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        List<Fragment> fragmentList = new ArrayList<>();
+
+        public void addFragment(Fragment fragment, String title){
+            arrayList.add(title);
+            fragmentList.add(fragment);
+        }
+
+        public MainAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+           return fragmentList.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return arrayList.get(position);
+        }
     }
 }
